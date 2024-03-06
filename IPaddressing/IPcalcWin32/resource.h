4ea59f40f0fa7452bcc076e040a43f7bfc2a0086
Clayman <Clayman@DESKTOP-DM8E77U>
2024-03-06 17:44:00 +0300

    IPcalcWin32 day 1

---
+++ IPaddressing/IPcalcWin32/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDC_STATIC_IP_ADDRESS           1001
+#define IDC_IPADDRESS                   1002
+#define IDC_STATIC_MASK                 1003
+#define IDC_IPMASK                      1004
+#define IDC_STATIC_PREFIX               1005
+#define IDC_EDIT_PREFIX                 1006
+#define IDC_SPIN_PREFIX                 1007
+#define IDC_STATIC_INFO                 1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
